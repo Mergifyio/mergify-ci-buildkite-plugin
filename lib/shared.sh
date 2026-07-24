86bc51db7756ee05f95ca1eb2e24425193f02cda
Joe Ippolite <101218846+joe-ipp@users.noreply.github.com>
2026-07-24 10:04:38 -0400

    Update shared.sh

--- lib/shared.sh
+++ lib/shared.sh
@@ -34,7 +34,9 @@ log_error() {
 
 # Resolve the Mergify token from plugin config or environment.
 resolve_token() {
+  set -x
   local token
   token="$(plugin_config TOKEN "${MERGIFY_TOKEN:-}")"
   echo "$token"
+  set +x
 }
